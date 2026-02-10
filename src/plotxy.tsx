5dc68eba3b4d3ce8b976dd2a6c734e94bda4dae1
Michael Pilosov <consistentbayes@gmail.com>
2026-02-10 14:34:30 -0700

    sci notation in number (log)

--- src/plotxy.tsx
+++ src/plotxy.tsx
@@ -215,6 +215,28 @@ export class PlotXY extends React.Component<PlotXYProps, PlotXYState> {
       }
       return d3.format(".3~e");
     }
+    function getTickValues(param: string, scale: any, approxTickCount: number): any[] | null {
+      const paramDef = me.props.params_def[param];
+      if (
+        !paramDef ||
+        paramDef.type !== ParamType.NUMERICLOG ||
+        typeof scale.ticks !== "function"
+      ) {
+        return null;
+      }
+      const ticks = scale.ticks(approxTickCount);
+      if (!Array.isArray(ticks)) {
+        return null;
+      }
+      const majorTicks = ticks.filter((tick: number) => {
+        if (!Number.isFinite(tick) || tick <= 0) {
+          return false;
+        }
+        const lg = Math.log10(tick);
+        return Math.abs(lg - Math.round(lg)) < 1e-10;
+      });
+      return majorTicks.length > 1 ? majorTicks : null;
+    }
     function redraw_axis() {
       me.svg.selectAll(".axis_render").remove();
       me.svg.selectAll(".brush").remove();
@@ -242,6 +264,10 @@ export class PlotXY extends React.Component<PlotXYProps, PlotXYState> {
       ]);
       const xTickFormatter = getTickFormatter(me.state.axis_x, x_scale);
       const yTickFormatter = getTickFormatter(me.state.axis_y, y_scale);
+      const yTickCount = 1 + me.state.height / 40;
+      const xTickCount = 1 + me.state.width / 80;
+      const xTickValues = getTickValues(me.state.axis_x, x_scale, xTickCount);
+      const yTickValues = getTickValues(me.state.axis_y, y_scale, yTickCount);
       zoom_brush = d3
         .brush()
         .extent([
@@ -256,7 +282,8 @@ export class PlotXY extends React.Component<PlotXYProps, PlotXYState> {
           .call(
             d3
               .axisLeft(y_scale)
-              .ticks(1 + me.state.height / 40)
+              .ticks(yTickCount)
+              .tickValues(yTickValues as any)
               .tickFormat(yTickFormatter as any)
               .tickSizeInner(margin.left + margin.right - me.state.width),
           )
@@ -289,7 +316,8 @@ export class PlotXY extends React.Component<PlotXYProps, PlotXYState> {
           .call(
             d3
               .axisBottom(x_scale)
-              .ticks(1 + me.state.width / 80)
+              .ticks(xTickCount)
+              .tickValues(xTickValues as any)
               .tickFormat(xTickFormatter as any)
               .tickSizeInner(margin.bottom + margin.top - me.state.height),
           )
